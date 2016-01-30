e15ad4eee726c131c60e61e5fc84f1a143e9e9ca
Trevor Lory <trevorlory@att.net>
2016-01-29 23:50:43 -0500

    made level class

---
+++ Game1/Game1/Level.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Game1
+{
+    class Level
+    {
+        int[][] level;
+
+        public Level()
+        {
+            
+        }
+
+        public void LoadLevel(char[] filename)
+        {
+
+        }
+
+        public void DestroyLevel()
+        {
+
+        }
+    }
+}
